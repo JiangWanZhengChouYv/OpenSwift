463f7c40edb4621e1d6996e03e48464cb40ec91a
JiangWanZhengChouYv <3327218513@qq.com>
2026-07-22 09:30:53 +0800

    chore: update HomeBrew Cask SHA256 for v0.1.1

--- Casks/openswift.rb
+++ Casks/openswift.rb
@@ -1,6 +1,6 @@
 cask "openswift" do
   version "0.1.1"
-  sha256 "TO_BE_COMPUTED_AFTER_RELEASE"
+  sha256 "4898ed2f87027946acdc3decea74acf822950e30c051db0c3e8ba622b05bc341"
 
   url "https://github.com/JiangWanZhengChouYv/OpenSwift/releases/download/v#{version}/OpenSwift-v#{version}.zip"
   name "OpenSwift"
