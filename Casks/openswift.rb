7df98dc2d865365864914897813f0de2e892285e
JiangWanZhengChouYv <3327218513@qq.com>
2026-08-06 11:32:45 +0800

    release: v2.0.0
    - Bump version to 2.0.0 in Info.plist and Cask
    - Update Cask SHA256 for new release asset

--- Casks/openswift.rb
+++ Casks/openswift.rb
@@ -1,6 +1,6 @@
 cask "openswift" do
-  version "1.0.0"
-  sha256 "412c8ad13e80c8eca56d3c8ca5cdfd2302d512e0c01586dc35b61e21088c7ad2"
+  version "2.0.0"
+  sha256 "2a5ba12977838b104f5c54b37fe78f270d11fab7261fd37be6ea1cfed8a455db"
 
   url "https://github.com/JiangWanZhengChouYv/OpenSwift/releases/download/v#{version}/OpenSwift-v#{version}.zip"
   name "OpenSwift"
